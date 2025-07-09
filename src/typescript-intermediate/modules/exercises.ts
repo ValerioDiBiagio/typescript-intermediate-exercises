01ad332ce9408e0d49226067508da7a4c5f70e91
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-09 11:57:07 +0200

    add first ex

---
+++ src/typescript-intermediate/modules/exercises.ts
@@ -0,0 +1,8 @@
+// CONSEGNA: importa le funzioni add e multiply da main.ts
+// // e usale per stampare:
+// // - 3 + 5 = 8
+// // - 4 * 6 = 24
+
+import { add, multiply } from "./utils/main";
+console.log(add(3, 5));
+console.log(multiply(4, 6));
