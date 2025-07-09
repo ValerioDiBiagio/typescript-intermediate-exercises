1a9729241bae38530144046982b905795b985037
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-09 12:14:58 +0200

    add third ex

--- src/typescript-intermediate/modules/exercises.ts
+++ src/typescript-intermediate/modules/exercises.ts
@@ -3,13 +3,24 @@
 // // - 3 + 5 = 8
 // // - 4 * 6 = 24
 
-import { add, multiply } from "./utils/main";
-console.log(add(3, 5));
-console.log(multiply(4, 6));
+// import { add, multiply } from "./utils/main";
+// console.log(add(3, 5));
+// console.log(multiply(4, 6));
 
 
 // importa la funzione greet da main.ts (usando default import)  e stampala con il nome "Lucia"
 
-import greet from "./utils/main"
-console.log(greet("Lucia"));
+// import greet from "./utils/main"
+// console.log(greet("Lucia"));
+
+
+// importa area come default e PI come named da main.ts
+// calcola area(5)
+// stampa PI
+
+import { PI } from "./utils/main";
+import area from "./utils/main";
+
+console.log(area(5));
+console.log(PI);
 
--- src/typescript-intermediate/modules/utils/main.ts
+++ src/typescript-intermediate/modules/utils/main.ts
@@ -1,19 +1,24 @@
 // EX 1
 
-export function add(a: number, b: number): number {
-    return a + b;
-}
+// export function add(a: number, b: number): number {
+//     return a + b;
+// }
 
-export function multiply(a: number, b: number): number {
-    return a * b;
-}
+// export function multiply(a: number, b: number): number {
+//     return a * b;
+// }
+
 
 // EX 2
 
-export default function greet(name: string): string {
-    return `Ciao, ${name} !`;
-}
+// export default function greet(name: string): string {
+//     return `Ciao, ${name} !`;
+// }
 
 // EX 3 
 
+export const PI = 3.14;
 
+export default function area(radius: number): number {
+    return PI * radius * radius;
+}
