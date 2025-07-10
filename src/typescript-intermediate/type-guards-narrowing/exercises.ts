8a4cd6b93e32d3f942da409cce11dce6b4e9c925
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-10 10:52:48 +0200

    add first ex

---
+++ src/typescript-intermediate/type-guards-narrowing/exercises.ts
@@ -0,0 +1,31 @@
+// Scrivi una funzione che prende un parametro input di tipo string | number.
+// Se è una stringa, stampa la sua lunghezza.
+// Se è un numero, stampa il numero arrotondato a due decimali.
+
+function start(input: string | number) {
+    if (typeof input === "string") {
+        console.log(input.length);
+    } else {
+        console.log(input.toFixed(2));
+    }
+}
+
+start("Ciao");
+start(123.45);
+
+
+
+// Scrivi una funzione chiamata controllaTipo che riceve un parametro value di tipo boolean, string o number.
+// Se value è un booleano, stampa "Valore booleano".
+// Se value è una stringa, stampa "Valore stringa".
+// Se value è un numero, stampa "Valore numero".
+
+
+
+
+// Scrivi una funzione chiamata stampaStringaOArray che riceve un parametro x di tipo string oppure array di stringhe (string[]).
+// Se x è una stringa, stampa "Singola stringa: " seguito dal valore di x.
+// Se x è un array di stringhe, stampa "Array di stringhe con lunghezza: " seguito dalla lunghezza dell’array.
+
+
+
