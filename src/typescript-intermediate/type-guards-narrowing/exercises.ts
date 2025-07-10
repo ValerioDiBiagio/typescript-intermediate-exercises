be87107cc254921a9ceab8e3eb5b56e05868f4b2
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-10 11:05:58 +0200

    add second ex

--- src/typescript-intermediate/type-guards-narrowing/exercises.ts
+++ src/typescript-intermediate/type-guards-narrowing/exercises.ts
@@ -14,13 +14,29 @@ start("Ciao");
 start(123.45);
 
 
-
 // Scrivi una funzione chiamata controllaTipo che riceve un parametro value di tipo boolean, string o number.
 // Se value è un booleano, stampa "Valore booleano".
 // Se value è una stringa, stampa "Valore stringa".
 // Se value è un numero, stampa "Valore numero".
 
 
+function controllaTipo(value: boolean | string | number) {
+    switch (typeof value) {
+        case "boolean":
+            console.log("Valore booleano");
+            break;
+        case "string":
+            console.log("Valore stringa");
+            break;
+        case "number":
+            console.log("Valore numero");
+            break;
+    }
+}
+
+controllaTipo(true);
+controllaTipo("Come stai?");
+controllaTipo(34);
 
 
 // Scrivi una funzione chiamata stampaStringaOArray che riceve un parametro x di tipo string oppure array di stringhe (string[]).
