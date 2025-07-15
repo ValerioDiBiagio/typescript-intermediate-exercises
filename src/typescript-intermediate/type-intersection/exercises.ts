755ae725d161fa9f03db71b56055293eadaf3fbd
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-15 13:07:13 +0200

    add third ex

--- src/typescript-intermediate/type-intersection/exercises.ts
+++ src/typescript-intermediate/type-intersection/exercises.ts
@@ -27,12 +27,12 @@
 }
 
 // Crea un tipo LoginData con le proprietà:
-// username: string
-// password: string
+// username:
+// password: 
 // Crea un tipo Profile con le proprietà:
-// name: string
-// age: number
-// Crea un tipo UserAccount che sia l’intersezione (&) tra LoginData e Profile.
+// name:
+// age: 
+// Crea un tipo UserAccount che sia l’intersezione tra LoginData e Profile.
 // Scrivi una funzione chiamata printAccount che:
 // accetta come parametro un oggetto di tipo UserAccount
 // stampa su console tutte le proprietà: username, password, name, age
@@ -70,3 +70,68 @@
     printAccount(user);
 
 }
+
+// Crea due tipi:
+// UserInfo con le proprietà:
+// name: string
+// email: string
+// Permissions con le proprietà:
+// isAdmin: boolean
+// canEdit: boolean
+// Crea un tipo AdminUser che sia l’intersezione tra UserInfo e Permissions.
+// Scrivi una funzione printPermissions che:
+// accetta un oggetto di tipo AdminUser
+// stampa:
+// "Admin: sì" se isAdmin è true
+// "Può modificare: sì" se canEdit è true
+// e i dati dell’utente (name, email)
+// Crea almeno due oggetti AdminUser diversi e chiamali nella funzione.
+
+{
+    type UserInfo = {
+        name: string;
+        email: string
+    }
+
+    type Permissions = {
+        isAdmin: boolean;
+        canEdit: boolean
+    }
+
+    type AdminUser = UserInfo & Permissions;
+
+    function printPermissions(admin: AdminUser): void {
+
+        console.log(admin.name);
+        console.log(admin.email);
+
+        if (admin.isAdmin) {
+            console.log("Admin: si");
+        } else {
+            console.log("Admin: no")
+        } if (admin.canEdit) {
+            console.log("Può modificare: si");
+        } else {
+            console.log("Può modificare: no")
+        }
+    }
+
+    const firstAdminUser: AdminUser = {
+        name: "Luigi",
+        email: "luigi@gmail.com",
+        isAdmin: true,
+        canEdit: true
+    }
+
+    const secondAdminUser: AdminUser = {
+        name: "Franco",
+        email: "franco@gmail.com",
+        isAdmin: false,
+        canEdit: true
+    }
+
+    printPermissions(firstAdminUser);
+    printPermissions(secondAdminUser);
+
+}
+
